9ab6c0fe06db10e7012bc75641152fa4e972df65
Ella Thompson <fang98t@gmail.com>
2021-01-18 11:14:13 -0500

    apiroute pseudocode

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,3 +2,13 @@ const Workout = require("../models/workout");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.post
+
+router.get
+
+router.get
+
+router.put
+
+module.exports = router;
