139cfd72c41c1ecb81fe31f384d3571cd9c3a95a
Ella Thompson <fang98t@gmail.com>
2021-01-18 11:30:25 -0500

    router get 1

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,7 +2,9 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 router.get
 
