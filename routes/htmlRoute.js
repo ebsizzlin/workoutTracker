369c2f882aa9fb6aa90a798efd8b00b8b04aafec
Ella Thompson <fang98t@gmail.com>
2021-01-18 11:31:12 -0500

    router get 2

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -6,7 +6,9 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 router.get
 
