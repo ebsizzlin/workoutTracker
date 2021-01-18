4f118dc1f0a84d7b5d95db16c3d325dc36b72f47
Ella Thompson <fang98t@gmail.com>
2021-01-18 11:03:39 -0500

    correcting models

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: { type: Date, deault: () => new Date() },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                },
+                name: {
+                    type: String,
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: Number,
+                reps: Number,
+                sets: Number,
+                distance: Number,
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    },
+);
+
+workoutSchema.virtual('totalDuration').get( () => {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
