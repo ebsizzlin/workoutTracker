3faadd1752f13230795a16b6ee36a2c1b6a0ce03
Ella Thompson <fang98t@gmail.com>
2021-01-23 23:02:07 -0500

    altering arrow func

--- models/workout.js
+++ models/workout.js
@@ -49,7 +49,7 @@ const workoutSchema = new Schema(
 );
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function() {
+workoutSchema.virtual("totalDuration").get(() => {
     
     // "reduce" array of exercises down to just the sum of their durations
     return this.exercises.reduce((total, exercise) => {
