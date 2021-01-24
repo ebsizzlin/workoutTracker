b57ae8c294a4d8a057a0d81273baf48c0348f145
Ella Thompson <fang98t@gmail.com>
2021-01-23 23:04:58 -0500

    changing htmlroute organization

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,18 +1,18 @@
 const path = require("path");
-const express = require("express");
-const Workout = require("../models/workout");
-const router = express.Router();
+// const express = require("express");
+// const Workout = require("../models/workout");
+// const router = express.Router();
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 module.exports = router;
